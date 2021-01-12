2bd227b0c5a68d589bd71aa407e90c0a079527b8
metanoia1989 <sogaxili@gmail.com>
2021-01-12 20:41:10 +0800

    添加出去配置的选项

--- SpringBootFullStack/demo/src/main/java/com/aop/demo/DemoApplication.java
+++ SpringBootFullStack/demo/src/main/java/com/aop/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.aop.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class}) // 除去某个配置
 public class DemoApplication {
 
 	public static void main(String[] args) {
