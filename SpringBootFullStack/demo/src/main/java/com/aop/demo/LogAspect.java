afd5281d9723f863d564e39b3a30f74935469ffe
metanoia1989 <sogaxili@gmail.com>
2021-01-12 20:35:00 +0800

    添加aop

---
+++ SpringBootFullStack/demo/src/main/java/com/aop/demo/LogAspect.java
@@ -0,0 +1,48 @@
+package com.aop.demo;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAspect {
+    @Pointcut("execution(* com.aop.demo.*.*(..))")    
+    public void pc1() {}
+    
+    @Before(value = "pc1()")
+    public void before(JoinPoint jp) {
+        String name = jp.getSignature().getName(); 
+        System.out.println(name + "方法开始执行...");
+    }
+
+    @After(value = "pc1()")
+    public void after(JoinPoint jp) {
+        String name = jp.getSignature().getName(); 
+        System.out.println(name + "方法执行结束...");
+    }
+
+    @AfterReturning(value = "pc1()", returning = "result")
+    public void afterReturning(JoinPoint jp, Object result) {
+        String name = jp.getSignature().getName(); 
+        System.out.println(name + "方法返回值为：" + result);
+    }
+
+    @AfterThrowing(value = "pc1()", throwing = "e")
+    public void afterThrowing(JoinPoint jp, Exception e) {
+        String name = jp.getSignature().getName(); 
+        System.out.println(name + "方法抛出了异常，异常是：" + e);
+    }
+    
+    @Around("pc1()")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        return pjp.proceed(); 
+    }
+}
---
+++ SpringBootFullStack/demo/src/main/java/com/aop/demo/UserController.java
@@ -0,0 +1,21 @@
+package com.aop.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService; 
+    
+    @GetMapping("/getUserById")
+    public String getUserById(Integer id) {
+        return userService.getUserById(id); 
+    }
+
+    @GetMapping("/deleteUserById")
+    public void deleteUserById(Integer id) {
+        userService.deleteUserById(id); 
+    }
+}
