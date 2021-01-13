38458ea5717cc47442bce111c190c7a732fe059d
metanoia1989 <sogaxili@gmail.com>
2021-01-13 21:21:02 +0800

    单元测试补全失败，明天继续

---
+++ SpringBootFullStack/chapter04_restfulservice/testdemo/src/main/java/com/testdemo/testdemo/server/HelloService.java
@@ -0,0 +1,10 @@
+package com.testdemo.testdemo.server;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String sayHello(String name) {
+        return "Hello " + name + " !"; 
+    } 
+}
